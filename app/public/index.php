948725ff62ed7b24c2ee8c504846c2a56dbf547a
masa <psychedelic.nekopunch@gmail.com>
2021-04-12 11:04:23 +0900

    add

---
+++ app/public/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html>
+<head>
+	<meta charset='utf8'>
+	<meta name='viewport' content='width=device-width'>
+	<title>js html sample</title>
+</head>
+
+<body>
+	<div id="app">sample</div>
+	<script type="text/javascript">
+		
+	</script>
+</body>
+</html>
